0e34d711e193f43da73491fc274e099b1c9f9fa0
AdarshVarma9395 <adarshvarma1999@gmail.com>
2024-01-17 16:38:07 +0530

    Django-froms

--- loginapp/views.py
+++ loginapp/views.py
@@ -1,15 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import CreateUserForm,LoginForm
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+
+
 
 def home(request):
     return render(request, "loginapp/index.html")
 
 def register(request):
-    return render(request, "loginapp/register.html")
+
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("my-login")
+        
+    context = {"registerform":form}
+
+    return render(request, "loginapp/register.html",context)
 
 
 def my_login(request):
-    return render(request, "loginapp/my-login.html")
+
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request,data = request.POST)
+        if form.is_valid():
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request,user)
+                return redirect("dashboard")
+    context = {"loginform" : form}
 
 
+    return render(request, "loginapp/my-login.html",context)
+
+
+
+def user_logout(request):
+    auth.logout(request)
+    return redirect("")
+
+@login_required(login_url="my-login")
 def dashboard(request):
     return render(request, "loginapp/dashboard.html")
